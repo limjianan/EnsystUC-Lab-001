15f1f5a5cdfcf23d14921e235e6344dd676f5dd9
limjianan <jianan.lim@gmail.com>
2018-07-23 16:40:03 +1000

    deployexchange.ps1

--- Single-Server-AD/DSC/deployExchange.ps1
+++ Single-Server-AD/DSC/deployExchange.ps1
@@ -1,4 +1,4 @@
-﻿configuration CreateADPDC-with-Data
+﻿configuration deployExchange
 {
    param
    (
@@ -12,7 +12,7 @@
         [Int]$RetryIntervalSec=30
     )
 
-    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, xNetworking, PSDesiredStateConfiguration, xPendingReboot,cChoco,xExchange
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface=Get-NetAdapter|Where-Object Name -Like "Ethernet*"|Select-Object -First 1
     $InterfaceAlias=$($Interface.Name)
@@ -24,37 +24,200 @@
             RebootNodeIfNeeded = $true
         }
 
-	    WindowsFeature DNS
+       #Installs Required Components for Exchange (note: there is 1 planned automatic reboot)
+        WindowsFeature ASHTTP
         {
-            Ensure = "Present"
-            Name = "DNS"
+            Ensure = 'Present'
+            Name = 'AS-HTTP-Activation'
         }
-
-        Script EnableDNSDiags
-	    {
-      	    SetScript = {
-		        Set-DnsServerDiagnostics -All $true
-                Write-Verbose -Verbose "Enabling DNS client diagnostics"
-            }
-            GetScript =  { @{} }
-            TestScript = { $false }
-	        DependsOn = "[WindowsFeature]DNS"
-        }
-
-	    WindowsFeature DnsTools
-	    {
-	        Ensure = "Present"
-            Name = "RSAT-DNS-Server"
-            DependsOn = "[WindowsFeature]DNS"
-	    }
-
-        xDnsServerAddress DnsServerAddress
+        WindowsFeature DesktopExp
         {
-            Address        = '127.0.0.1'
-            InterfaceAlias = $InterfaceAlias
-            AddressFamily  = 'IPv4'
-	        DependsOn = "[WindowsFeature]DNS"
+            Ensure = 'Present'
+            Name = 'Desktop-Experience'
         }
+         WindowsFeature NetFW45
+        {
+            Ensure = 'Present'
+            Name = 'NET-Framework-45-Features'
+        }
+           WindowsFeature RPCProxy
+        {
+            Ensure = 'Present'
+            Name = 'RPC-over-HTTP-proxy'
+        }
+            WindowsFeature RSATClus
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-Clustering'
+        }
+            WindowsFeature RSATClusCmd
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-Clustering-CmdInterface'
+        }
+            WindowsFeature RSATClusMgmt
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-Clustering-Mgmt'
+        }
+           WindowsFeature RSATClusPS
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-Clustering-PowerShell'
+        }
+           WindowsFeature WebConsole
+        {
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Console'
+        }
+            WindowsFeature WAS
+        {
+            Ensure = 'Present'
+            Name = 'WAS-Process-Model'
+        }
+            WindowsFeature WebAsp
+        {
+            Ensure = 'Present'
+            Name = 'Web-Asp-Net45'
+        }
+           WindowsFeature WBA
+        {
+            Ensure = 'Present'
+            Name = 'Web-Basic-Auth'
+        }
+           WindowsFeature WCA
+        {
+            Ensure = 'Present'
+            Name = 'Web-Client-Auth'
+        }
+          WindowsFeature WDA
+        {
+            Ensure = 'Present'
+            Name = 'Web-Digest-Auth'
+        }
+          WindowsFeature WDB
+        {
+            Ensure = 'Present'
+            Name = 'Web-Dir-Browsing'
+        }
+           WindowsFeature WDC
+        {
+            Ensure = 'Present'
+            Name = 'Web-Dyn-Compression'
+        }
+           WindowsFeature WebHttp
+        {
+            Ensure = 'Present'
+            Name = 'Web-Http-Errors'
+        }
+           WindowsFeature WebHttpLog
+        {
+            Ensure = 'Present'
+            Name = 'Web-Http-Logging'
+        }
+           WindowsFeature WebHttpRed
+        {
+            Ensure = 'Present'
+            Name = 'Web-Http-Redirect'
+        }
+          WindowsFeature WebHttpTrac
+        {
+            Ensure = 'Present'
+            Name = 'Web-Http-Tracing'
+        }
+          WindowsFeature WebISAPI
+        {
+            Ensure = 'Present'
+            Name = 'Web-ISAPI-Ext'
+        }
+          WindowsFeature WebISAPIFilt
+        {
+            Ensure = 'Present'
+            Name = 'Web-ISAPI-Filter'
+        }
+            WindowsFeature WebLgcyMgmt
+        {
+            Ensure = 'Present'
+            Name = 'Web-Lgcy-Mgmt-Console'
+        }
+            WindowsFeature WebMetaDB
+        {
+            Ensure = 'Present'
+            Name = 'Web-Metabase'
+        }
+            WindowsFeature WebMgmtSvc
+        {
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Service'
+        }
+           WindowsFeature WebNet45
+        {
+            Ensure = 'Present'
+            Name = 'Web-Net-Ext45'
+        }
+            WindowsFeature WebReq
+        {
+            Ensure = 'Present'
+            Name = 'Web-Request-Monitor'
+        }
+             WindowsFeature WebSrv
+        {
+            Ensure = 'Present'
+            Name = 'Web-Server'
+        }
+              WindowsFeature WebStat
+        {
+            Ensure = 'Present'
+            Name = 'Web-Stat-Compression'
+        }
+               WindowsFeature WebStatCont
+        {
+            Ensure = 'Present'
+            Name = 'Web-Static-Content'
+        }
+               WindowsFeature WebWindAuth
+        {
+            Ensure = 'Present'
+            Name = 'Web-Windows-Auth'
+        }
+              WindowsFeature WebWMI
+        {
+            Ensure = 'Present'
+            Name = 'Web-WMI'
+        }
+              WindowsFeature WebIF
+        {
+            Ensure = 'Present'
+            Name = 'Windows-Identity-Foundation'
+        }
+              WindowsFeature RSATADDS
+        {
+            Ensure = 'Present'
+            Name = 'RSAT-ADDS'
+        }
+	cChocoInstaller installChoco
+        { 
+            InstallDir = "C:\choco"
+        }
+
+        cChocoFeature allowGlobalConfirmation {
+            FeatureName = "allowGlobalConfirmation"
+            Ensure = 'Present'
+	    DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+        cChocoPackageInstaller ucma4 {
+            FeatureName = "ucma4"
+            Ensure = 'Present'
+	    DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+        cChocoPackageInstaller vcredist2013 {
+            FeatureName = "vcredist2013"
+            Ensure = 'Present'
+	    DependsOn = "[cChocoInstaller]installChoco"
+        }
+
 
         xWaitforDisk Disk2
         {
@@ -75,27 +238,7 @@
             DriveLetter = "F"
         }
 #>
-        WindowsFeature ADDSInstall
-        {
-            Ensure = "Present"
-            Name = "AD-Domain-Services"
-	        DependsOn="[WindowsFeature]DNS"
-        }
-
-        WindowsFeature ADDSTools
-        {
-            Ensure = "Present"
-            Name = "RSAT-ADDS-Tools"
-            DependsOn = "[WindowsFeature]ADDSInstall"
-        }
-
-        WindowsFeature ADAdminCenter
-        {
-            Ensure = "Present"
-            Name = "RSAT-AD-AdminCenter"
-            DependsOn = "[WindowsFeature]ADDSInstall"
-        }
-
+  
         xADDomain FirstDS
         {
             DomainName = $DomainName
@@ -115,12 +258,6 @@
             DependsOn = "[xADDomain]FirstDS"
         }
 
-        xADRecycleBin RecycleBin
-        {
-           EnterpriseAdministratorCredential = $DomainCreds
-           ForestFQDN = $DomainName
-           DependsOn = '[xWaitForADDomain]DscForestWait'
-        }
 
         ### OUs ###
         $DomainRoot = "DC=$($DomainName -replace '\.',',DC=')"
